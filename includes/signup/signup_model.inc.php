29068f2c98ce821463fc8d648bb470ffa27bad39
Maanasa Poluparti <maanasap@usf.edu>
2023-11-25 03:08:00 -0500

    Add files via upload

--- includes/signup/signup_model.inc.php
+++ includes/signup/signup_model.inc.php
@@ -4,7 +4,8 @@
 declare(strict_types=1);
 //Don't need to require link to database, because we link it in signup.inc.php 
 
-function get_username(object $pdo, string $username) {
+function get_username(object $pdo, string $username)
+{
     $query = "SELECT username FROM users WHERE username = :username;";
     $stmt = $pdo->prepare($query); //prevents SQL injection
     $stmt->bindParam(":username", $username);
@@ -14,7 +15,8 @@ function get_username(object $pdo, string $username) {
     return $result;
 }
 
-function get_email(object $pdo, string $email) {
+function get_email(object $pdo, string $email)
+{
     $query = "SELECT email FROM users WHERE email = :email;";
     $stmt = $pdo->prepare($query); //prevents SQL injection
     $stmt->bindParam(":email", $email);
@@ -24,11 +26,21 @@ function get_email(object $pdo, string $email) {
     return $result;
 }
 
-function set_user(object $pdo, string $pwd, string $email, string $user_type) {
+function set_user(object $pdo, string $pwd, string $email, string $user_type)
+{
     $query = "INSERT INTO users (email, pwd, user_type) VALUES (:email, :pwd, :user_type);";
     $stmt = $pdo->prepare($query); //prevents SQL injection
     $stmt->bindParam(":email", $email);
     $stmt->bindParam(":pwd", $pwd);
     $stmt->bindParam(":user_type", $user_type);
     $stmt->execute();
+
+    if ($user_type == "Pet Owner") {
+        $user_id = $pdo->lastInsertId();
+        $query = "INSERT INTO customers (user_id, email) VALUES (:user_id, :email);";
+        $stmt = $pdo->prepare($query); //prevents SQL injection
+        $stmt->bindParam(":user_id", $user_id);
+        $stmt->bindParam(":email", $email);
+        $stmt->execute();
+    }
 }
