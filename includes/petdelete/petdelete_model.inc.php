89aed4f4a0e028ed9f4ea8e77ef3a7b1783c7466
Maanasa Poluparti <maanasap@usf.edu>
2023-11-26 15:13:58 -0500

    Update petdelete_model.inc.php

--- includes/petdelete/petdelete_model.inc.php
+++ includes/petdelete/petdelete_model.inc.php
@@ -3,12 +3,12 @@ declare(strict_types=1);
 
 function get_pets(object $pdo, string $user_id)
 {
-    $query = "SELECT name, weight, age FROM Animals WHERE owner = :user_id;";
+    $query = "SELECT * FROM Animals WHERE owner = :user_id;";
     $stmt = $pdo->prepare($query); //prevents SQL injection
     $stmt->bindParam(":user_id", $user_id);
     $stmt->execute();
 
-    $result = $stmt->fetch(PDO::FETCH_ASSOC);
+    $result = $stmt->fetchAll(PDO::FETCH_ASSOC);
     return $result;
 }
 
@@ -23,9 +23,21 @@ function customer_exists(object $pdo, string $user_id)
     return ($result !== false);
 }
 
+function pet_exists(object $pdo, string $user_id, string $name)
+{
+    $query = "SELECT name FROM Animals WHERE owner = :user_id and LOWER(name) = LOWER(:name);";
+    $stmt = $pdo->prepare($query); //prevents SQL injection
+    $stmt->bindParam(":user_id", $user_id);
+    $stmt->bindParam(":name", $name);
+    $stmt->execute();
+
+    $result = $stmt->fetch(PDO::FETCH_ASSOC);
+    return ($result !== false);
+}
+
 function delete_pet(object $pdo, string $user_id, string $name)
 {
-    $query = "DELETE FROM Animals WHERE owner = :user_id AND name = :name;";
+    $query = "DELETE FROM Animals WHERE owner = :user_id AND LOWER(name) = LOWER(:name);";
     $stmt = $pdo->prepare($query); //prevents SQL injection
     $stmt->bindParam(":user_id", $user_id);
     $stmt->bindParam(":name", $name);
