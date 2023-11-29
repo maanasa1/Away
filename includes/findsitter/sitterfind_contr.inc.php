d9933b76f049a216f838f57117c489ff473c8862
Zhanna Sergeeva <94724268+zhannasunny@users.noreply.github.com>
2023-11-29 18:29:58 -0500

    Add files via upload

--- includes/findsitter/sitterfind_contr.inc.php
+++ includes/findsitter/sitterfind_contr.inc.php
@@ -3,18 +3,18 @@
 function find_available_sitters(object $pdo, int $zipcode, array $availableDays, array $availableTimes, array $sizePref, array $typePref) {
     $query = "SELECT * FROM Sitters 
     WHERE zipcode = :zipcode 
-    AND ARRAY[:availableDays] <@ string_to_array(available_days, ',')
-    AND ARRAY[:availableTimes] <@ string_to_array(available_times, ',')
-    AND ARRAY[:sizePref] <@ string_to_array(size_pref, ',')
-    AND ARRAY[:typePref] <@ string_to_array(type_pref, ',')";
+    AND ARRAY[:availableDays] && string_to_array(available_days, ',')
+    AND ARRAY[:availableTimes] && string_to_array(available_times, ',')
+    AND ARRAY[:sizePref] && string_to_array(size_pref, ',')
+    AND ARRAY[:typePref] && string_to_array(type_pref, ',')";
 
-    $stmt = $pdo->prepare($query);
+    $stmt = $pdo->prepare($query); 
 
     $stmt->bindParam(':zipcode', $zipcode);
-    $stmt->bindValue(':availableDays', implode(",", $availableDays));
-    $stmt->bindValue(':availableTimes', implode(",", $availableTimes));
-    $stmt->bindValue(':sizePref', implode(",", $sizePref));
-    $stmt->bindValue(':typePref', implode(",", $typePref));
+    $stmt->bindParam(':availableDays', implode(",", $availableDays));
+    $stmt->bindParam(':availableTimes', implode(",", $availableTimes));
+    $stmt->bindParam(':sizePref', implode(",", $sizePref));
+    $stmt->bindParam(':typePref', implode(",", $typePref));
 
     $stmt->execute();
 
