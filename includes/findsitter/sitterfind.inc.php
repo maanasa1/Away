d93dd43f821f7e93b3711ee5870e0aa77ac0b893
Zhanna Sergeeva <94724268+zhannasunny@users.noreply.github.com>
2023-11-29 14:20:53 -0500

    Add files via upload

--- includes/findsitter/sitterfind.inc.php
+++ includes/findsitter/sitterfind.inc.php
@@ -2,7 +2,7 @@
 if ($_SERVER["REQUEST_METHOD"] == "POST") {
     require_once '../config_session.inc.php';
     $user_id = $_SESSION["user_id"];
-    $zipcode = $_POST["zipcode"];
+    $zipcode = htmlspecialchars($_POST["zipcode"]);
     $availableDays = $_POST["days"];
     $availableTimes = $_POST["time"];
     $sizePref = $_POST["size_pref"];
@@ -10,7 +10,7 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
 
     try {
         require_once '../dbh.inc.php';
-        require_once 'sitterfind_model.inc.php';
+        
         require_once 'sitterfind_contr.inc.php';
 
         $availableSitters = find_available_sitters($pdo, $zipcode, $availableDays, $availableTimes, $sizePref, $typePref);
--- includes/findsitter/sitterfind_contr.inc.php
+++ includes/findsitter/sitterfind_contr.inc.php
@@ -1,22 +1,20 @@
 <?php
+
 function find_available_sitters(object $pdo, int $zipcode, array $availableDays, array $availableTimes, array $sizePref, array $typePref) {
     $query = "SELECT * FROM Sitters 
-              WHERE zipcode = :zipcode 
-              AND string_to_array(available_days, ',') <@ ARRAY[:availableDays]
-              AND string_to_array(available_times, ',') <@ ARRAY[:availableTimes]
-              AND string_to_array(size_pref, ',') <@ ARRAY[:sizePref]
-              AND string_to_array(type_pref, ',') <@ ARRAY[:typePref]";
+    WHERE zipcode = :zipcode 
+    AND ARRAY[:availableDays] <@ string_to_array(available_days, ',')
+    AND ARRAY[:availableTimes] <@ string_to_array(available_times, ',')
+    AND ARRAY[:sizePref] <@ string_to_array(size_pref, ',')
+    AND ARRAY[:typePref] <@ string_to_array(type_pref, ',')";
 
-    
     $stmt = $pdo->prepare($query);
 
     $stmt->bindParam(':zipcode', $zipcode);
-    // Use bindValue to pass values directly
-    $stmt->bindValue(':availableDays', implode(", ", $availableDays));
-    $stmt->bindValue(':availableTimes', implode(", ", $availableTimes));
-    $stmt->bindValue(':sizePref', implode(", ", $sizePref));
-    $stmt->bindValue(':typePref', implode(", ", $typePref));
-
+    $stmt->bindValue(':availableDays', implode(",", $availableDays));
+    $stmt->bindValue(':availableTimes', implode(",", $availableTimes));
+    $stmt->bindValue(':sizePref', implode(",", $sizePref));
+    $stmt->bindValue(':typePref', implode(",", $typePref));
 
     $stmt->execute();
 
